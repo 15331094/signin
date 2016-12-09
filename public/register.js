e605f84cb1c9f1ccf28cf8aa910e03bfce9905e7
一条咸鱼的自我修养 <1617899539@qq.com>
2016-12-09 21:50:55 +0800

    Update register.js

--- public/register.js
+++ public/register.js
@@ -49,16 +49,16 @@ $(document).ready(function() {
 	});
 	//当点击提交按钮
 	$("#submit").click(function() {
-		alert("点击了注册按钮");
+		//alert("点击了注册按钮");
 		//当个人信息合法且还没被注册过，则注册成功，并把信息上传到server.js
 		if(checkName($("#name").val()) == 1 && checkID($("#ID").val()) == 1 && 
 			checkPhone($("#phone").val()) == 1 && checkEmail($("#email").val()) == 1 && 
 			checkPassword($("#password").val()) == 1) {
-			alert("现在提交看看");
+			//alert("现在提交看看");
 			$.post("../process_post", {"name":$("#name").val(), "ID":$("#ID").val(), 
             "phone":$("#phone").val(), "email":$("#email").val(), "password":$("#password").val()}, function(data) {
-				alert("lala");
-				alert(data);
+				//alert("lala");
+				//alert(data);
 				$(".tips").text(data);
 				if(data == "注册成功!") {
 					//alert("lala");
@@ -92,6 +92,7 @@ $(document).ready(function() {
 		$("#hide").css("display", "block");
 		$("#Show").css("display", "block");
 		$("#movie").css("display", "block");
+		$("#movie").css("top", "50px");
 		show(window.location.search.substring(10));
 	}
 
